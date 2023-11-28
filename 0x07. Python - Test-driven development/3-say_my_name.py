7cab8d3dca9aa8ad19c22a9a9c46abc61ae825a2
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-11-28 22:25:36 +0200

    Task 3

---
+++ 0x07. Python - Test-driven development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The 3-say_my_name module for printing name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints My name is <first name> <last name>
+    Args:
+        -fist_name (str): the first name
+        -last_name (str): the last name
+    Raises:
+        -TypeError: check for invalid input
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
