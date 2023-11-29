d2f38fbaaae0cd8fca496e62affd2343f946b0f2
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-11-29 09:25:32 +0200

    Please take a rest

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """
-5-text_indentation module
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
 """
 
 
 def text_indentation(text):
-    """
-    Function that print text
-    Args:
-        -text (str): text to be printed
-    Raises:
-        -TypeError: check for string
-    """
-    if not isinstance(text, str):
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    flag = 0
-    for char in text:
-        if flag == 0:
-            if char == " ":
+    zz = 0
+    for a in text:
+        if zz == 0:
+            if a == ' ':
                 continue
             else:
-                flag = 1
-        if flag == 1:
-            if char in ["?", ".", ":"]:
-                print(char)
+                zz = 1
+        if zz == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
                 print()
-                flag = 0
+                zz = 0
             else:
-                print(char, end="")
+                print(a, end="")
