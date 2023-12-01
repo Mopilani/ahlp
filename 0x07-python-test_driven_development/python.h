8ea88de5e69bc2e404345e543e7a71ff5241c35c
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-12-01 21:05:11 +0200

    Task 102 of test driven dev

---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,11 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_python_string(PyObject *p);
+
+#endif /*_PYTHON_H_*/
